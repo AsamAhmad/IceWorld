55e17d3a030d31bf2c00935760f350e1187eef02
Farid <47351903+farid78320@users.noreply.github.com>
2019-05-05 17:56:04 +0200

    html dashboard
    un peu d'intégration

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -10,9 +10,40 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractController
 {
+/**
+ * @Route("accueil-dashboard", name="accueil-dashboard")
+ * Page D'accueil
+ */
+
+    public function index()
+
+    {
+        return $this->render("dashboard/accueil-dashboard.html.twig");
+    }
+
+
+
+/**
+ * @Route("produits", name="gestion des produits")
+ * Gestion des Produits
+ */
+
+
+    public function produits()
+
+    {
+        return $this->render("dashboard/produits.html.twig");
+    }
+
+
+
+
+
+
 
 
 }
